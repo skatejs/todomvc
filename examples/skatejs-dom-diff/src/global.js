aa7fecdc3db1f50826f9bcd03b7edfb6e419cbb8
Trey Shugart <treshugart@gmail.com>
2016-05-20 20:13:17 +1000

    Example isn't necessary after merging Kickflip into Skate.

--- examples/skatejs-dom-diff/src/global.js
+++
@@ -1,2 +0,0 @@
-import app from './index';
-window.app = app;
--- examples/skatejs-dom-diff/src/index.js
+++
@@ -1,8 +0,0 @@
-import 'todomvc-common/base';
-import TodoApp from './todo-app';
-import TodoStore from './todo-store';
-
-export default {
-	TodoApp,
-	TodoStore
-};
